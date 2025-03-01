627f51daad67b83ebdf208415264e2dc72a542b5
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-03-01 17:53:05 +0530

    Practice

---
+++ DaysSwitch.js
@@ -0,0 +1,36 @@
+//using switch case print days 
+
+let day = 7;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("tuesday");
+        break;
+
+    case 3:
+        console.log("wednesday");
+        break;
+
+    case 4:
+        console.log("Thusday");
+        break;
+
+    case 5:
+        console.log("friday");
+        break;
+
+    case 6:
+        console.log("saturday");
+        break;
+
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        break;
+}
