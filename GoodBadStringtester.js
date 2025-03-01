9d79f90fb15af95f5c13fa418e611a3a1071d354
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-03-01 16:47:01 +0530

    Practice

---
+++ GoodBadStringtester.js
@@ -0,0 +1,11 @@
+// A good string start with the letter 'a','A' and length of a string >3 .WAP to find string Good or Bad
+
+str = "ajay"
+console.log(`The string are ${str}`);
+
+if (str[0]=='a' && str.length > 3) {      //check the statments
+    console.log("Good String");
+}
+else{
+    console.log("Bad string");
+}
